51f011bfdb78d9697566de82961ad5ef2f5ae654
kevdliu <kevdliu@gmail.com>
2013-08-08 21:19:30 +0200

    gps.conf tweaks

--- ftl.mk
+++ ftl.mk
@@ -2,4 +2,5 @@ SUPERUSER_PACKAGE := com.ftl.superuser
 
 # Copy FTL Customs
 PRODUCT_COPY_FILES += \
-    vendor/FTL/prebuilt/bootanimation.zip:system/media/bootanimation.zip
+    vendor/FTL/prebuilt/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/FTL/prebuilt/gps.conf:system/etc/gps.conf
